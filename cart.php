037031ccafc5d2819a2d0363c40c2d94dd7e703a
Laurentiu Dodoloi <dodoloilaurentiu@gmail.com>
2018-08-20 11:39:59 +0300

    translate labels

--- cart.php
+++ cart.php
@@ -75,9 +75,9 @@
     <a href="/"><?= translate("Go to index"); ?></a>
 
     <form method="POST" action="">
-        <input type="text" name="name" placeholder="Name">
-        <input type="text" name="email" placeholder="Email">
-        <textarea cols="20" rows="5" name="comments" placeholder="Comments"></textarea>
+        <input type="text" name="name" placeholder="<?= translate("Name");?>">
+        <input type="text" name="email" placeholder="<?= translate("Email");?>">
+        <textarea cols="20" rows="5" name="comments" placeholder="<?= translate("Comments");?>"></textarea>
 
         <input type="submit" value="<?= translate("Checkout");?>">
     </form>
