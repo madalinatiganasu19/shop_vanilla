b2cf4647f0f5e3d10c0fc8168c21c48c6b284d7c
Laurentiu Dodoloi <dodoloilaurentiu@gmail.com>
2018-08-21 13:35:58 +0300

    #6: login.php http://pm.5psolutions.net/public/index.php?path_info=projects/training-madalina-tiganasu/tasks/6 add login form

--- login.php
+++ login.php
@@ -5,6 +5,12 @@
 
 <?php require_once("inc/header.php"); ?>
 
-    
+    <form action="" method="POST">
+        <input type="text" name="email" placeholder="<?= translate("Email"); ?>" value="<?= isset($_POST["login"]) ? $_POST["email"] : ""; ?>">
+        <input type="password" name="password" placeholder="<?= translate("Password"); ?>">
+
+        <input type="submit" name="login" value="<?= translate("Login"); ?>">
+        <p><?= $err; ?></p>
+    </form>
 
 <?php require_once("inc/footer.php"); ?>
