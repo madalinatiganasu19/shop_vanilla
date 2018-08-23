b42fe6f6ab2f5462702980f39d1829477974734c
Laurentiu Dodoloi <dodoloilaurentiu@gmail.com>
2018-08-23 10:37:20 +0300

    #7: products.php http://pm.5psolutions.net/public/index.php?path_info=projects/training-madalina-tiganasu/tasks/7 fetch all data

--- products.php
+++ products.php
@@ -1,10 +1,36 @@
 <?php
     require_once('common.php');
 
+    $sql = "SELECT * FROM products";
+    $result = $db->query($sql);
+
 
 ?>
 
 <?php require_once("inc/header.php"); ?>
 
     
+
+    <table>
+        <?php foreach($result as $row): ?>
+            <tr>
+                <td><img src="images/<?= $row['image']; ?>"></td>
+                <td>&nbsp;&nbsp;&nbsp;</td>
+                <td>
+                    <p><?= $row['title']; ?></p>
+                    <p><?= wordwrap($row['description'],50,"<br>\n",TRUE); ?></p>
+                    <p>
+                        <?= translate("$"); ?>
+                        <?= $row['price']; ?>
+                    </p>
+                </td>
+                <td>&nbsp;&nbsp;&nbsp;</td>
+                <td>
+                    <p><a href="product.php?id=<?= $row['id']; ?>"><?= translate("UPDATE"); ?></a></p>
+                    <p><a href="?id=<?= $row['id']; ?>"><?= translate("DELETE"); ?></a></p>
+                </td>
+            </tr>
+        <?php endforeach; ?>
+    </table>
+
 <?php require_once("inc/footer.php"); ?>
