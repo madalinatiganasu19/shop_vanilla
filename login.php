0c73dd2c156556992c4302adc06dc19db8ed2c52
Laurentiu Dodoloi <dodoloilaurentiu@gmail.com>
2018-08-23 14:46:44 +0300

    #6: login.php http://pm.5psolutions.net/public/index.php?path_info=projects/training-madalina-tiganasu/tasks/6 remove htmlspecialchars

--- login.php
+++ login.php
@@ -22,7 +22,7 @@
 <?php require_once("inc/header.php"); ?>
 
     <form method="POST">
-        <input type="text" name="email" placeholder="<?= translate("Email"); ?>" value="<?= isset($_POST["login"]) ? htmlspecialchars($_POST["email"]) : ""; ?>">
+        <input type="text" name="email" placeholder="<?= translate("Email"); ?>" value="<?= isset($_POST["login"]) ? $_POST["email"] : ""; ?>">
         <input type="password" name="password" placeholder="<?= translate("Password"); ?>">
 
         <input type="submit" name="login" value="<?= translate("Login"); ?>">
