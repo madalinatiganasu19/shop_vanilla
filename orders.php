f69c0a0f730f47492dc90f93c06af217e51938ee
Laurentiu Dodoloi <dodoloilaurentiu@gmail.com>
2018-08-28 12:30:45 +0300

    #9: orders.php http://pm.5psolutions.net/public/index.php?path_info=projects/training-madalina-tiganasu/tasks/9 fetch data

--- orders.php
+++ orders.php
@@ -1,2 +1,38 @@
 <?php
+    require_once("common.php");
+
+    security_check();
+
+    $sql = "SELECT o.id, o.name, o.email, SUM(p.price) AS total
+            FROM orders AS o
+            JOIN orders_products AS op ON o.id = op.order_id
+            JOIN products AS p ON op.product_id = p.id
+            GROUP BY o.id;";
+
+    $result = $db->query($sql);
 ?>
+
+<?php require_once("inc/header.php"); ?>
+
+    <table>
+        <tr>
+            <th>NAME</th>
+            <td>&nbsp;</td>
+            <th>EMAIL</th>
+            <td>&nbsp;</td>
+            <th>TOTAL</th>
+        </tr>
+        <?php foreach($result as $row): ?>
+            <tr>
+                <td><?= $row['name']; ?></td>
+                <td>&nbsp;</td>
+                <td><?= $row['email']; ?></td>
+                <td>&nbsp;</td>
+                <td><?= $row['total']; ?></td>
+                <td>&nbsp;</td>
+                <td><a href="order.php?id=<?=$row['id'];?>">View Order</a></td>
+            </tr>
+        <?php endforeach; ?>
+    </table>
+
+<?php require_once("inc/footer.php"); ?>
