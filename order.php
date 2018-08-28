ce4e010560baf69400adff135685fae550d16983
Laurentiu Dodoloi <dodoloilaurentiu@gmail.com>
2018-08-28 12:48:48 +0300

    #9: orders.php http://pm.5psolutions.net/public/index.php?path_info=projects/training-madalina-tiganasu/tasks/9 show order details

--- order.php
+++ order.php
@@ -3,12 +3,41 @@
 
     security_check();
 
+    if (isset($_GET['id'])) {
+        $id = $_GET['id'];
 
+
+        $stmt = "SELECT p.* FROM products AS p
+                JOIN orders_products AS op ON op.product_id = p.id
+                WHERE op.order_id = ?;";
+
+        $stmt = $db->prepare($stmt);
+        $stmt->bind_param('i', $id);
+        $stmt->execute();
+        $result = $stmt->get_result();
+    }
 
 ?>
 
 <?php require_once("inc/header.php"); ?>
 
-    
+    <table>
+        <?php foreach($result as $row) : ?>
+
+            <tr>
+                <td><img src="images/<?= $row['image']; ?>"></td>
+                <td>&nbsp;&nbsp;&nbsp;</td>
+                <td>
+                    <p><?= $row['title']; ?></p>
+                    <p><?= $row['description']; ?></p>
+                    <p>
+                        <?= translate("$"); ?>
+                        <?= $row['price']; ?>
+                    </p>
+                </td>
+            </tr>
+
+        <?php endforeach; ?>
+    </table>
 
 <?php require_once("inc/footer.php"); ?>
